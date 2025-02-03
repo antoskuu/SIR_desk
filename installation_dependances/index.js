0197a9edeceb774852ceb3bb1e21094b7b3fa15d
antoskuu <antoning.antonin@gmail.com>
2025-02-03 17:12:50 +0100

    removed useless packets

--- installation_dependances/index.js
+++ installation_dependances/index.js
@@ -1,9 +1,5 @@
 import { NRRDLoader } from 'three/addons/loaders/NRRDLoader.js';
-import * as fflate from 'three/addons/libs/fflate.module.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { VolumeRenderShader1 } from 'three/addons/shaders/VolumeShader.js';
 
 window.NRRDLoader = NRRDLoader;
-window.fflate = fflate;
-window.OrbitControls = OrbitControls;
 window.VolumeRenderShader1 = VolumeRenderShader1;
